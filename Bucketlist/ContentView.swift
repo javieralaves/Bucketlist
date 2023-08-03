ebe230ec2f9d837231fabcca21ecf5ff926c4ddb
Javier Alaves <jayalaves@gmail.com>
2023-08-03 12:47:49 +0200

    Main view styling

--- Bucketlist/ContentView.swift
+++ Bucketlist/ContentView.swift
@@ -5,17 +5,41 @@
 //  Created by Javier Alaves on 3/8/23.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0),
+                                                      span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Map(coordinateRegion: $mapRegion)
+                .ignoresSafeArea()
+            
+            Circle()
+                .fill(.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button {
+                        // create new location
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    .padding()
+                    .background(.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                }
+            }
         }
-        .padding()
     }
 }
 
