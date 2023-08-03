2f06f4596b676295210b7df65edcb92a091a6d2c
Javier Alaves <jayalaves@gmail.com>
2023-08-03 13:08:41 +0200

    Location struct improvemetns

--- Bucketlist/Location.swift
+++ Bucketlist/Location.swift
@@ -5,6 +5,7 @@
 //  Created by Javier Alaves on 3/8/23.
 //
 
+import CoreLocation
 import Foundation
 
 struct Location: Identifiable, Codable, Equatable {
@@ -13,4 +14,19 @@ struct Location: Identifiable, Codable, Equatable {
     var description: String
     let latitude: Double
     let longitude: Double
+    
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    static let example = Location(id: UUID(),
+                                  name: "Buckingham Palace",
+                                  description: "Where Queen Elizabeth lives with her dorgis",
+                                  latitude: 51.501,
+                                  longitude: -0.141)
+    
+    // To compare locations by their ID
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
 }
