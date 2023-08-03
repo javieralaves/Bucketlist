e1f53c3bebeb5519db107c4eedc6fd6bfaebafd8
Javier Alaves <jayalaves@gmail.com>
2023-08-03 12:32:50 +0200

    Initial Commit

---
+++ Bucketlist/BucketlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketlistApp.swift
+//  Bucketlist
+//
+//  Created by Javier Alaves on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
