d37f33821179efe44fd8ec87d32df80ce43ee50b
Javier Alaves <jayalaves@gmail.com>
2023-08-04 15:56:37 +0200

    Dynamically find nearby places using Wikipedia API

---
+++ Bucketlist/Result.swift
@@ -0,0 +1,22 @@
+//
+//  Result.swift
+//  Bucketlist
+//
+//  Created by Javier Alaves on 4/8/23.
+//
+
+import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+}
